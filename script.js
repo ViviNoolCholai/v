1c6985ab482991664ddb1fcfd0cf6a4288635a96
rocky2210 <lilavathra@gmail.com>
2025-06-12 09:19:33 +0000

    fix:Added mobile bg color

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 if(window.innerWidth <= 768){
     document.body.classList.remove('bg-gradient-to-br','from-purple-800','via-purple-900','to-indigo-900');
-    document.body.classList.add('bg-gradient-to-br','from-purple-900','via-purple-950','to-indigo-950');
+    document.body.classList.add('bg-gradient-to-br','from-purple-600','via-purple-650','to-indigo-650');
 }
